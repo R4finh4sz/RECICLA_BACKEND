b939244e86653fcaa0de4e9dd9a937db21d9204e
gustavorisio <spgustavorisio@gmail.com>
2026-02-18 19:24:21 -0300

    Feat: Added Auth-Security in Collection Types

---
+++ src/api/auth-security/controllers/auth-security.ts
@@ -0,0 +1,7 @@
+/**
+ * auth-security controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::auth-security.auth-security');
---
+++ src/api/auth-security/services/auth-security.ts
@@ -0,0 +1,7 @@
+/**
+ * auth-security service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::auth-security.auth-security');
