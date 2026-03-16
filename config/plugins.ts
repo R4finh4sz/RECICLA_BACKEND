8f9a2b57fbc3479661b91c1a817318fda8f3f25e
gustavorisio <spgustavorisio@gmail.com>
2026-03-15 23:24:53 -0300

    Fix: fix SMTP issues with Render

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,8 +6,8 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
       provider: 'nodemailer',
       providerOptions: {
         host: env('SMTP_HOST'),
-        port: env.int('SMTP_PORT', 465),
-        secure: env.int('SMTP_PORT', 465) === 465,
+        port: env.int('SMTP_PORT', 587),
+        secure: env.int('SMTP_PORT', 587) === 587,
         auth: {
           user: env('SMTP_USER'),
           pass: env('SMTP_PASS'),
