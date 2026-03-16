386bb724de62790aa4e58323211ad5f730b02cbe
gustavorisio <spgustavorisio@gmail.com>
2026-03-15 22:20:53 -0300

    Fix: update nodemailer SMTP, package.json and strapi provider email

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,24 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT', 465),
+        secure: env.int('SMTP_PORT', 465) === 465,
+        auth: {
+          user: env('SMTP_USER'),
+          pass: env('SMTP_PASS'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM', env('SMTP_USER')),
+        defaultReplyTo: env('SMTP_REPLY_TO', env('SMTP_FROM', env('SMTP_USER'))),
+      },
+    },
+  },
+});
 
 export default config;
